7cf9068d22de605165eaf3a2cb9628ced94177a6
Bohdan <bgbg78915@gmail.com>
2022-12-09 21:55:47 +0200

    09.12

--- game2/main_game.py
+++ game2/main_game.py
@@ -43,7 +43,6 @@ def run_game():
                 if event.type == pygame.QUIT:
                     game = False  
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    print(33333)
                     click = event.pos 
                     if settings.play.RECT.collidepoint(click):
                         print(1)
@@ -76,6 +75,9 @@ def run_game():
             pygame.display.flip()
             # pass
         if level1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game = False  
             smoke_count += 1
             settings.bg.blit_sprite(win)
             sprite.sprite.can_move_right(area.list_rect)
@@ -90,12 +92,13 @@ def run_game():
             sprite.mask.gravity(list_rect= area.list_rect)
             settings.lever.blit_sprite(win)
             settings.injured.blit_sprite(win)
+      
             for el in area.list_create_world:
                 el.blit_sprite(win)
             # if smoke_count == 50:
             #     # sprite.smoke.WIDTH += 50
             #     # sprite.smoke.HEIGHT += 50
-            #     # print(1)
+            #     print(1)
             #     smoke_width += 50
             #     smoke_height += 50
             #     # smoke_x += 20
@@ -103,9 +106,7 @@ def run_game():
             #     smoke = sprite.Sprite(x = smoke_x, y = smoke_y, width = smoke_width, height = smoke_height, name_image = "game2/images/smoke.png")
             #     # smoke.blit_sprite(win)
             #     smoke_count = 0
-            # smoke.blit_sprite(win)
-            if sprite.sprite.RECT.colliderect(smoke.RECT):
-                print(2222222)
+            smoke.blit_sprite(win)
             pygame.display.flip()
         if scene3:
             settings.bg_developers.blit_sprite(win)
